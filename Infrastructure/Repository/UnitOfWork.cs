4d8aeba80ad53ce81a10eaa2535a439958613e57
Andri <yawan.andrisetyawan@gmail.com>
2025-06-10 21:20:51 +0700

    Ticket View Part 1

--- Infrastructure/Repository/UnitOfWork.cs
+++ Infrastructure/Repository/UnitOfWork.cs
@@ -14,6 +14,7 @@ namespace Infrastructure.Repository
 		public UnitOfWork(AppDbContext context, ITicketRepository ticketRepository)
 		{
 			this.context = context;
+			this.TicketRepository = ticketRepository;
 		}
 		public async Task<int> SaveChanges()
 		{
